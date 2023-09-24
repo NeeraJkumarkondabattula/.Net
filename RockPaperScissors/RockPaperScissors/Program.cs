094674c84ed385f4008285a2b456682c98681184
Neeraj Kumar Kondabattula <119170814+NeeraJkumarkondabattula@users.noreply.github.com>
2023-09-24 11:27:38 +0530

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool play = true;
+            while (play)
+            {
+                String you = "";
+                int com = random.Next(1, 4);
+                String computer = "";
+                while (you != "ROCK" && you != "PAPER" && you != "SCISSORS")
+                {
+                    Console.Write("Choose ROCK,PAPER or SCISSCORS : ");
+                    you = Console.ReadLine();
+                    you = you.ToUpper();
+
+                }
+                switch (com)
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+
+                Console.WriteLine("Computer : " + computer);
+
+                switch (you)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("its draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("you lose");
+                        }
+                        else
+                        {
+                            Console.WriteLine("you win!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("its win!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("you draw");
+                        }
+                        else
+                        {
+                            Console.WriteLine("you lose");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("its lose");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("you win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("you draw");
+                        }
+                        break;
+                }
+
+                Console.Write("do you want to play again ? (Y/N) ");
+                String playagain = Console.ReadLine();
+                playagain = playagain.ToUpper();
+
+                if (playagain == "N")
+                {
+                    play = false;
+                }
+            }
+        }
+    }
+}
